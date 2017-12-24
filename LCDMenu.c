5bcf862c82d51212ac72fd07dd788a2f5a36f40c
Albert Nadal <a.nadal@iskra.cat>
2017-12-24 12:53:21 +0100

    Added menu demo

--- LCDMenu.c
+++ LCDMenu.c
@@ -5,18 +5,18 @@
 #define TOTAL_OPTIONS 3
 
 typedef const struct menu_option {
-        const int id;
-        const int parent_id;
+  const int id;
+  const int parent_id;
 	const char title[16];
 	void (*on_select)(void);
 } MenuOption;
 
 typedef struct menu_node {
-	char title[16];
-        struct menu_node *next_node;
-        struct menu_node *previous_node;
-        struct menu_node *parent_node;
-        struct menu_node *child_node;
+  char title[16];
+  struct menu_node *next_node;
+  struct menu_node *previous_node;
+  struct menu_node *parent_node;
+  struct menu_node *child_node;
 	void (*on_select)(void);
 } MenuNode;
 
@@ -27,19 +27,27 @@ typedef struct lcd_menu {
 } LCDMenu;
 
 void move_to_next_option(LCDMenu* lcd_menu) {
-	
+  if(lcd_menu->current_node->next_node != NULL) {
+    lcd_menu->current_node = lcd_menu->current_node->next_node;
+  }
 }
 
 void move_to_previous_option(LCDMenu* lcd_menu) {
-
+  if(lcd_menu->current_node->previous_node != NULL) {
+    lcd_menu->current_node = lcd_menu->current_node->previous_node;
+  }
 }
 
 void move_to_parent_option(LCDMenu* lcd_menu) {
-
+  if(lcd_menu->current_node->parent_node != NULL) {
+    lcd_menu->current_node = lcd_menu->current_node->parent_node;
+  }
 }
 
 void move_to_child_option(LCDMenu* lcd_menu) {
-
+  if(lcd_menu->current_node->child_node != NULL) {
+    lcd_menu->current_node = lcd_menu->current_node->child_node;
+  }
 }
 
 void select_current_option(LCDMenu* lcd_menu) {
@@ -56,6 +64,7 @@ MenuNode* init_menu_with_parent_node(MenuOption options[], int parent_option_id,
 		if(options[i].parent_id == parent_option_id)
 		{
 			MenuNode *node = malloc(sizeof(MenuNode));
+
 			strcpy(node->title, options[i].title);
 			if(previous_node != NULL) {
 				previous_node->next_node = node;
@@ -64,6 +73,7 @@ MenuNode* init_menu_with_parent_node(MenuOption options[], int parent_option_id,
 			} else {
 				first_node = node;
 			}
+      node->next_node = NULL;
 			node->previous_node = previous_node;
 			node->parent_node = parent_node;
 			node->on_select = options[i].on_select;
@@ -81,6 +91,14 @@ void init_menu_with_options(LCDMenu* lcd_menu, MenuOption options[]) {
 	lcd_menu->current_node = root_node;
 }
 
+void show_menu(LCDMenu* lcd_menu) {
+  MenuNode* current_node = lcd_menu->current_node;
+  if(current_node != NULL) {
+    printf("%s\n", lcd_menu->title);
+    printf("%s\n", current_node->title);
+  }
+}
+
 LCDMenu* create_empty_menu() {
 	LCDMenu *menu = malloc(sizeof(struct lcd_menu));
 	menu->title[0] = '\0';
@@ -90,12 +108,23 @@ LCDMenu* create_empty_menu() {
 }
 
 LCDMenu* create_empty_menu_with_title(char* title) {
-        LCDMenu *menu = create_empty_menu();
+  LCDMenu *menu = create_empty_menu();
 	strcpy(menu->title, title);
-        return menu;
+  return menu;
+}
+
+unsigned int get_pressed_key() {
+  unsigned int ch;
+  while(1) {
+    ch = getchar();
+    if(ch != '\n') {
+      return ch;
+    }
+  }
 }
 
 int main(int argc, char *argv[]) {
+  unsigned int key;
 	printf("LCDMenu\n\n");
 	LCDMenu *menu = create_empty_menu_with_title("Menu principal");
 
@@ -104,6 +133,29 @@ int main(int argc, char *argv[]) {
 				 	     { .id = 3, .parent_id = 1, .title = "Option C\0", .on_select = NULL }};
 
 	init_menu_with_options(menu, options);
+
+  while(1) {
+    show_menu(menu);
+    switch(key = get_pressed_key()) {
+      case 119: printf("w\n"); // UP
+                move_to_previous_option(menu);
+                break;
+      case 115: printf("s\n"); // DOWN
+                move_to_next_option(menu);
+                break;
+      case 97:  printf("a\n"); // LEFT
+                move_to_parent_option(menu);
+                break;
+      case 100: printf("d\n"); // RIGHT
+                move_to_child_option(menu);
+                break;
+      case 32:  printf("space\n"); // SPACE
+                select_current_option(menu);
+                break;
+
+    }
+
+  }
+
 	return 0;
 }
-
